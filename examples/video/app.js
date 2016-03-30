7833b91566913a858217b11c9e96a871e844d444
Shavit <stmmsn@gmail.com>
2016-03-30 05:39:59 +0300

    starting with the examples 🗒

--- examples/video/app.js
+++ examples/video/app.js
@@ -3,19 +3,20 @@ $(document).ready(function(){
   function loadWebCams(){
     $("div[data-webcam]").webcam({
       append: false,
-      width: 640,
-      height: 480,
-      mode: "callback",
+      width: 324,
+      height: 240,
+      mode: "save",
       swffile: "../../jscam.swf",
-      quality: 10,
+      quality: 60,
 
       onSave: function(data){
         var col = data.split(";");
-        console.log("On Save");
       },
+
       onCapture: function(){
         webcam.save();
       },
+
       onLoad: function () {
           var cams = webcam.getCameraList();
           for(var i in cams) {
@@ -26,5 +27,9 @@ $(document).ready(function(){
   }
 
   loadWebCams();
+
+  $("#capture").on("click", function(event){
+    console.log(webcam.capture("/camera/save"));
+  });
 
 });
